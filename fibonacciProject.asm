ffdafd5146549c8a2c1c86eea7aaf0ba5d9b9478
ashley <90946757+ashleyh12@users.noreply.github.com>
2022-12-01 21:13:11 -0500

    printing the entered fibonacci number, reading string

--- fibonacciProject.asm
+++ fibonacciProject.asm
@@ -4,4 +4,10 @@ enter_fib_number: .asciiz “Insert a positive integer greater than or equal to 
 return_fib_number: .asciiz "Your Fibonacci number is:\n"
 error: .asciiz “That is an illegal number\n”
 
-
+.text
+# printing enter_fib_number
+li $v0, 4
+la $a0, enter_fib_number
+syscall
+# reading the string entered by user
+li $v0, 5
