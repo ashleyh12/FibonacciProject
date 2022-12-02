5fc6e1077397d6ccce0465ef862f4ed82282a8aa
ashley <90946757+ashleyh12@users.noreply.github.com>
2022-12-01 21:08:38 -0500

    Setting up stuff within the program

--- fibonacciProject.asm
+++ fibonacciProject.asm
@@ -1,1 +1,7 @@
+.data
+
+enter_fib_number: .asciiz “Insert a positive integer greater than or equal to 25:\n“
+return_fib_number: .asciiz "Your Fibonacci number is:\n"
+error: .asciiz “That is an illegal number\n”
+
 
