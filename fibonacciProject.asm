dd4ee7e6169d62588dd33f9af5c21af3f8b13812
ashley <90946757+ashleyh12@users.noreply.github.com>
2022-12-04 19:42:57 -0500

    Update fibonacciProject.asm

--- fibonacciProject.asm
+++ fibonacciProject.asm
@@ -31,21 +31,4 @@ move $a0, $a1
 #exit function
 li $v0, 10
 syscall
-.end main
 
-fib:
-addi $sp, $sp, -12
-sw $ra, 8($sp)
-sw $s0, 4($sp)
-sw $s1, 0($sp)
-move $s0, $a0
-li $v0, 1
-ble $s0, 0x2
-addi $a0, $s0, -1
-jal fib
-move $s1, $v0
-addi $a0, $s0, -2
-jal fib
-add $v0, $s1, $v0
-fibExit:
-lw $ra, 8($sp)
