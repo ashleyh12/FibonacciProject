ff89aa0fca58b90e620fbbfdaff90b8d31871949
ashley <90946757+ashleyh12@users.noreply.github.com>
2022-12-04 19:55:22 -0500

    moved lines 42-44 around

--- fibonacciProject.asm
+++ fibonacciProject.asm
@@ -37,26 +37,27 @@ syscall
 
 
 fib:
-addi $sp, $sp, -12
-sw $ra, 8($sp)
-sw $s0, 4($sp)
-sw $s1, 0($sp)
-move $s0, $a0
-li $v0, 1
-ble $s0, 0x2, fibExit
-addi $a0, $s0, -1
-jal fib
-move $s1, $v0
-addi $a0, $s0, -2
-jal fib
-add $v0, $s1, $v0
-fibExit:
-#end of program
-lw $ra, 8($sp)
-lw $s0, 4($sp)
-lw $s1, 0($sp)
-addi $sp, $sp, 12
-jr $ra
-#finished the fib program
+
+  addi $sp, $sp, -12
+  sw $s1, 8($sp)
+  sw $s0, 4($sp)
+  sw $ra, 0($sp)
+  move $s0, $a0
+  li $v0, 1
+  ble $s0, 0x2, fibExit
+  addi $a0, $s0, -1
+  jal fib
+  move $s1, $v0
+  addi $a0, $s0, -2
+  jal fib
+  add $v0, $s1, $v0
+  fibExit:
+  #end of program
+  lw $ra, 8($sp)
+  lw $s0, 4($sp)
+  lw $s1, 0($sp)
+  addi $sp, $sp, 12
+  jr $ra
+  #finished the fib program
 
 
